2b51df44a633ccce76b200293e1c95a5bc4553dd
alvarogm789 <111319483+alvarogm789@users.noreply.github.com>
2024-06-05 09:09:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -29,35 +29,3 @@ const dbconnect = () => {
 
 module.exports = dbconnect;
 
-
-/*
-const mongoose = require("mongoose");
-
-const dbconnect = () => {
-  mongoose.set("strictQuery", true);
-
-  return new Promise((resolve, reject) => {
-    mongoose
-      .connect("mongodb://localhost:27017/creinnovadb", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        console.log("**********************************************")
-        console.log("*                                            *")
-        console.log("*    Conexión exitosa a la base de datos     *")
-        console.log("*                                            *")
-        console.log("**********************************************")
-        resolve();
-      })
-      .catch((error) => {
-        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        console.error("|||  Error de conexión a la base de datos:", error, "|||");
-        console.log("¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡");
-        reject(error);
-      });
-  });
-};
-
-module.exports = dbconnect;
-*/
